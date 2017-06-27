7459b5cef9c76a68bb0017cd6c88adfc3ba55123
James Usrey <mrusogi@gmail.com>
2017-06-27 14:43:05 -0600

    Add project files.

---
+++ dcUriHelper/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace dcUriHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] ProgramArgs = args[0].Split(':');
+
+                if (ProgramArgs[0] == "winbox") {
+                    //string[] PassthroughArgs = ProgramArgs[1].Split(',');
+                    Process winBox = new Process();
+                    winBox.StartInfo.FileName = @"C:\winbox.exe";
+                    winBox.StartInfo.Arguments = ProgramArgs[1] + " DrillCommNOC mvgp97c";
+                    winBox.Start();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
